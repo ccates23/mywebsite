031f3d52d984ea87aa529e27f9cf8b709f8a4cc2
chadcates <cates.chad@gmail.com>
2015-09-09 16:16:44 -0500

    added jquery and made static header and footer with icons

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// jQuery for smooth page scroll to an anchor tag
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+      var target = this.hash;
+      var $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 1000, 'swing');
+  });
+});
